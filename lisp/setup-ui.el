cd8e3174ae825652724fbceb1056a8a678a7ebf3
gaoguoxing <nanyiniu76@gmail.com>
2025-05-23 17:28:38 +0800

    update cursor type

--- lisp/setup-ui.el
+++ lisp/setup-ui.el
@@ -8,6 +8,9 @@
 (require 'init-const)
 (require 'init-funcs)
 
+;; cursor type set hbar
+(setq-default cursor-type 'bar)
+
 ;; For all-the-icons
 (use-package all-the-icons
   :ensure t
@@ -75,11 +78,9 @@
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  
   ;; Treemacs theme
   (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
-  
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
